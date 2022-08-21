061b0f9236e7a3cf774210f5e83b6b094fd3b3c7
qua-ghe-gom <ntu25042006@gmail.com>
2022-08-21 23:39:50 +0700

    ka ka ka

---
+++ ex4/btvn/app.js
@@ -0,0 +1,13 @@
+let firstname = "tú";
+let surname = "minh";
+let lastname="nguyễn"
+var ok1=lastname+" "+surname+" "+firstname;
+var ok2="độ dài của chuỗi 'nguyễn minh tú' là: " +ok1.length
+document.getElementById("test").innerHTML = ok1;
+
+var mod = 8 % 3;
+let met="số dư của 8%3 là: "+mod
+document.getElementById("mid").innerHTML = met;
+
+document.getElementById("test3").innerHTML = ok2;
+
