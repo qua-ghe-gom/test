e822205a20f7a050321dcea1c4b6afa5217a41da
qua-ghe-gom <ntu25042006@gmail.com>
2022-09-18 11:07:59 +0700

    ok

---
+++ ex1.js
@@ -0,0 +1,34 @@
+let name1=function(name){
+    
+    document.write("hello "+name+" ! <br>")
+
+}
+
+name1("tuan khi")
+name1("tuan khi")
+name1("tuan khi")
+name1("tuan khi")
+function min(a,b){
+    return a<b ? a:b;
+    
+}
+document.write(min(1,2),"<br>");
+document.write(min(5,8),"<br>");
+document.write(min(17,28),"<br>");
+document.write(min(81,27),"<br>");
+document.write(min(3,27),"<br>");
+function max(a,b){
+    document.write(a+b+a+a+b,"<br>")
+}
+max(1,2)
+max(1,8)
+max(2,2)
+
+
+
+document.getElementById("ok").innerHTML="ghê hông ghê hông"
+document.getElementById("ok").style.background="red"
+
+    const myh1=document.getElementsByClassName("ko");
+    myh1[0].style.color="red";
+    myh1[1].style.color="blue";
