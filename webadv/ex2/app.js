b8b7e0956a795e6f2e806cad5ccdc73addc74e50
qua-ghe-gom <ntu25042006@gmail.com>
2022-11-26 20:54:14 +0700

    ok

---
+++ webadv/ex2/app.js
@@ -0,0 +1,35 @@
+//while
+/* let i=1;
+do {
+    document.write("số i là: ",i ,"<br>");
+    i++;
+}
+while (i<0); */
+//do while
+let i=1;
+/* do {
+    document.write("số i là: ",i ,"<br>");
+    i++;
+}
+while (i<0); */
+//for
+/* for (let i=1;i<5;i++){
+    document.write(" giá trị của i: ",i,"<br>")
+} */
+
+//object trong js
+//sd trong từ khoá js
+/* const dog={
+    name:"alaska",
+    age:"3 tuổi",
+    kg:"50kg",
+}
+for(const key in dog){
+console.log(key);
+console.log(dog[key]);
+} */
+
+
+//mảng
+/* const arr=[1,2,3]
+console.log(arr); */
